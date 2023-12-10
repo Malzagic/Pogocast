4ac2a50fbb4e10f624316c6728e5312a10911261
Malzagic <malzagic@MacBook-Pro-Malzagic.local>
2023-12-10 03:46:12 +0100

    adding data to forecast weather

--- src/components/UX/Card.jsx
+++ src/components/UX/Card.jsx
@@ -1,13 +1,19 @@
 import React from "react";
+import { TbTemperatureCelsius } from "react-icons/tb";
 
-const Card = ({src, hour, temp}) => {
+const Card = ({ src, hour, temp, description }) => {
+  const temperature = Math.round(temp - 273.15).toFixed(0);
   return (
     <div className="p-4 bg-blue-600 border border-blue-800 rounded-lg shadow-md m-2">
-      <div className="flex justify-between">
-        <img src={src} alt="forecast icon" />
-        <div className="flex flex-col">
+      <div className="flex justify-between items-center">
+        <img src={src} alt={description} width={50} height={50} />
+        <p className="text-sm text-slate-100">{description}</p>
+        <div className="flex flex-col items-end">
           <p className="text-slate-100">{hour}</p>
-          <p className="text-slate-100">{temp}</p>
+          <div className="flex flex-row items-center">
+            <p className="text-slate-100">{temperature}</p>
+            <TbTemperatureCelsius className="text-xl text-slate-100 ml-1" />
+          </div>
         </div>
       </div>
     </div>
--- src/components/pages/ForecastReport.jsx
+++ src/components/pages/ForecastReport.jsx
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { FetchingData } from "../hook/fetchData";
+import { Geolocation } from "../hook/geolocation";
 import Card from "../UX/Card";
+import CircleLoader from "react-spinners/ClipLoader";
 
 const ForecastReport = () => {
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(true);
   const src = "#";
   const hour = "14:00";
   const temp = "32 C";
 
-  return (
+  const dataHandler = async () => {
+    try {
+      const geolocaton = new Geolocation();
+      const coords = await geolocaton.start();
+      if (coords) {
+        const urls =
+          process.env.REACT_APP_WEATHER_URL +
+          `forecast?lat=${coords.lat}&lon=${coords.lon}`;
+        const fetchingData = new FetchingData(urls);
+        const res = await fetchingData.get();
+
+        setData(res);
+      }
+    } catch (err) {
+      console.error("Error getting coordinates:", err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    dataHandler();
+  }, []);
+
+  return loading && !data ? (
+    <div className="center">
+      <CircleLoader color="#f1f5f9" size={200} />
+    </div>
+  ) : (
     <main className="container mx-auto my-2 overflow-hidden pb-12">
       <div className="flex justify-between items-center">
         <div className="flex mx-auto">
@@ -17,22 +51,36 @@ const ForecastReport = () => {
         </div>
       </div>
       <div className="flex flex-col justify-center">
-        <Card src={src} hour={hour} temp={temp} />
+        {data.list.length > 0 && (
+          <Card
+            key={uuidv4()}
+            src={`https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`}
+            hour={data.list[0].dt_txt}
+            temp={data.list[0].main.temp}
+            description={data.list[0].weather[0].description}
+          />
+        )}
       </div>
       <div className="flex justify-between items-center">
         <div className="flex mx-auto">
           <h5 className="text-slate-300">Next forecast</h5>
         </div>
         <div className="flex mx-auto">
-          <h5 className="text-slate-300">5-days</h5>
+          <h5 className="text-slate-300">5-days/6:00-00:00</h5>
         </div>
       </div>
       <div className="flex flex-col justify-center">
-        <Card src={src} hour={hour} temp={temp} />
-        <Card src={src} hour={hour} temp={temp} />
-        <Card src={src} hour={hour} temp={temp} />
-        <Card src={src} hour={hour} temp={temp} />
-        <Card src={src} hour={hour} temp={temp} />
+        {data.list
+          .filter((item, index) => index > 0)
+          .map((item) => (
+            <Card
+              key={uuidv4()}
+              src={`https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`}
+              hour={item.dt_txt}
+              temp={item.main.temp}
+              description={item.weather[0].description}
+            />
+          ))}
       </div>
     </main>
   );
